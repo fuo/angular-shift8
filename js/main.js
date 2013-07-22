f2580c0067175f60902c1c8eb6b36848677d2440
Tom Maiaroto <tom@shift8creative.com>
2013-07-22 16:35:47 -0500

    adding angular-ui to enhance demos

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 /* 
  * Angular - Shift8 demo/documentation app.
  */
-angular.module('demoApp', ['shift8.bootstrap.form'], function($locationProvider) {
+angular.module('demoApp', ['ui', 'ui.jq', 'ui.bootstrap', 'ui.validate', 'shift8.bootstrap.form'], function($locationProvider) {
 	$locationProvider.hashPrefix('');
 	// Make code pretty
 	window.prettyPrint && prettyPrint();
@@ -27,17 +27,22 @@ var MainCtrl = function($scope, $element) {
 	$scope.DemoFormSchema = {
 		Name: {
 			type: 'text',
-			label: 'Your Name',
-			placeholder: 'John Doe'
+			label: 'Your Name'
 		},
 		Email: {
 			label: 'E-mail',
-			placeholder: 'jdoe@example.com',
 			helpBlock: '<small>Some help text here...Which can be inline or block by defining <code>help-block</code> or <code>help-inline</code> attributes.</small>'
 		},
 		Colors: {
 			label: 'Select A Color',
 			options: {"red": {"option": "Red"}, 'blueGreen': {"option": "Blue Green"}, 'blue': {"option": "Blue"}}
+		}
+	};
+	
+	$scope.validate = {
+		email: function(val) {
+			var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+			return re.test(val);
 		}
 	};
 
